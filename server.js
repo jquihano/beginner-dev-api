a28271f662328fb6b01c46d343530a94a2fefdc3
jquihano <jeremy.quihano@gmail.com>
2022-09-19 16:16:54 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const PORT = 1337;
+
+app.use(cors());
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port: ${PORT}`)
+})
