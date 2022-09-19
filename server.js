875bb40b86b3c08a24c48eab243ef68749bffe52
jquihano <jeremy.quihano@gmail.com>
2022-09-19 16:21:14 -0700

    Setting up get route for api

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
 const PORT = 1337;
 
 app.use(cors());
@@ -9,6 +8,10 @@ app.use(cors());
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
 });
+
+app.get('/api/:tag', (req, res)=> {
+    console.log(req.params.tag)
+})
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is running on port: ${PORT}`)
